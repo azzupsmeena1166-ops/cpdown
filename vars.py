36bbef98303bb1f5465bb2c5a79342e616e3a65f
azzupsmeena1166-ops <azzupsmeena1166@gmail.com>
2025-11-06 14:47:46 +0530

    Add setup instructions to vars.py
    Added installation instructions and usage notes.

--- vars.py
+++ vars.py
@@ -1,7 +1,6 @@
-api_id = 24320085 #change_with_your_Api_id.
-api_hash = "a9d8e5464a5ac65180bcb48fda55ec66" #change_with_your_Api_hash.
-bot_token = "" #change_with_your_bot_token.
-auth_users = [] #change_with_your_user_id.
-sudo_user =  #change_with_your_user_id.
-log_channel =  #change_with_your_channel_id.
-txt_channel =  #change_with_your_channel_id.
+sudo apt update
+git clone https://github.com/rishavdevkr/cpdown
+cd cpdown
+pip3 install -r requirements.txt
+python3 main.py
+Note:- use (nohup python3 main.py) to run 24×7
